2ce740436240d2bcf54a88a7de6599c649ca8c9e
Pawel Jaworski <javorski.pavel@gmail.com>
2021-08-30 23:23:39 +0200

    BaseCore project nest/tailwind

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = 
+{
+  images: {
+    domains: [
+      "links.papareact.com",
+      "platform-lookside.fbsbx.com",
+      "firestorage.googleapis.com",
+    ]
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
