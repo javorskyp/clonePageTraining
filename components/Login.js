2f69e7747b1005e54ff639592ce41e781c2f7ccb
Pawel <javorski.pavel@gmail.com>
2021-12-09 23:58:29 +0100

    imports

--- components/Login.js
+++ components/Login.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import { signIn } from "next-auth/client"
 
-function Login() {
-  return (
+export const Login = () => 
+   (
     <div className="grid place-items-center">
       <Image 
         src="https://links.papareact.com/t4i" 
@@ -13,6 +13,3 @@ function Login() {
       <h1 onClick={signIn} className="p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer">Login with Facebook</h1>
     </div>
   )
-}
-
-export default Login
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { getSession } from 'next-auth/client';
 import Head from 'next/head';
-import Header from '../components/Header';
-import Login from '../components/Login';
+import { Header } from '../components/Header';
+import { Login } from '../components/Login';
 
 export default function Home({ session }) {
   if (!session) return <Login/>
